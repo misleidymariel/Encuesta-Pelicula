ef90bd6a922f101758321e0b6211d2a710a5963a
Misleidy <misleidy@MacBook-Pro-de-Misleidy.local>
2020-04-23 11:05:11 -0300

    terminacion de la funcion mostrar competencias

--- servidor/controladores/competencias.js
+++ servidor/controladores/competencias.js
@@ -5,7 +5,7 @@ var database = new Database(config);
 
 function cargarCompetencias(req, res) {
 
-    database.query('SELECT * FROM competencia;').then( rows => {
+    database.query('SELECT * FROM competencia;').then(rows => {
         res.send(rows);
     });
 
@@ -13,22 +13,35 @@ function cargarCompetencias(req, res) {
 
 };
 
-function obtenerDosPeliculas(req , res){
+function obtenerDosPeliculas(req, res) {
 
     var id = req.params.id;
-    console.log(id);
     var peliculas;
     var competencia;
-    var promise = database.query('select *, RAND() as random from pelicula order by random desc limit 2;');
-    promise.then( peliculasRows => {
-        peliculas = peliculasRows;
-        var query = 'select * from competencia where id = ?'
-        return database.query(query, id);
-    }).then( competenciaRow => {
+    var query = 'select * from competencia where id = ?'
+    var promise = database.query(query, id);
+    
+    promise.then(competenciaRow => {
+        
+        if(competenciaRow.length == 0) {
+            var message = `No existe competenica con el id ${id}`;
+            return new Promise(( resolve, reject ) => {
+                var error = {status: 404, message }
+                reject(error);
+            });
+        }
+
         competencia = competenciaRow[0].nombre;
+        var query = 'select * from competencia where id = ?'
+        query = 'select *, RAND() as random from pelicula order by random desc limit 2;';
+        return database.query(query);
+    }).then(peliculasRows => {
+        peliculas = peliculasRows;
     }).then(() => {
         var opciones = { peliculas, competencia };
         res.send(opciones);
+    }).catch(error => {
+        res.status(error.status).send(error);
     });
 
     return res;
