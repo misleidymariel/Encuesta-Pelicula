baf284c5d5116ca53b4406b990ff57efed4f7851
Misleidy <misleidy@MacBook-Pro-de-Misleidy.local>
2020-08-03 20:45:11 -0300

    configure env db

--- servidor/lib/config.js
+++ servidor/lib/config.js
@@ -1,9 +1,9 @@
 var config = {
-    host     : 'localhost',
+    host     : process.env.DB_HOST,
     port     : 3306,
-    user     : 'pelis',
-    password : 'Control123',
-    database : 'competencias'
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 }
 
 module.exports = config
